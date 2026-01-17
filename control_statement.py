cad2cd3c1bd4d1e250cba831d73d4a2efe1e4c5b
ashish <ashishagnihotri996@gmail.con>
2026-01-18 00:03:51 +0530

    while with examples

--- control_statement.py
+++ control_statement.py
@@ -3,9 +3,43 @@
 # Output:
 # 5 10 15 20 25 30 35 40 45 50
 
-def multiplicationTable(N):
-    return [i * N for i in range(1, 11)]
+# def multiplicationTable(N):
+#     return [i * N for i in range(1, 11)]
+#
+# multiplicationTable(5)
 
-multiplicationTable(5)
+# Input: s = "DoctorPhenomenal"
+# Output: DcoPeoea
+
+# s = "DoctorPhenomenal"
+# result=""
+# for i in range(len(s)):
+#     if i%2==0:
+#         result +=s[i]
+# print(result)
+
+# Input: x = 3
+# Output: 3 2 1 0
+# Explanation:
+# Numbers in decreasing order from 3
+# are 3, 2, 1, 0.
+
+# x= 3
+# while x>=0:
+#     print(x,end=" ")
+#     x-=1
+
+# Input: x = 10
+# Output: 1 4 9
+# Explanation:From 1 to 10, numbers in powers of 2 are, 12, 22, 32 as 1, 4 and 9.
+
+# x =10
+# i = 0
+# while i*i<x:
+#     print(i*i,end="")
+#     i+=1
+
+
+
 
 
